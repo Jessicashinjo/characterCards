55ebc86f4f5829c08f99220019dc58774a006017
Chase Ramsey <ramsey.chase@gmail.com>
2016-04-19 20:01:22 -0500

    Created variables charCards, newCard, and cluster; populated cluster with HTML code chunks

--- cards.js
+++ cards.js
@@ -2,7 +2,42 @@
 // Create an array of objects named "charCards" and populate the array with a few premade objects.
 // Include key/value pairs for all card specifications that need to be stored.
 
+var charCards = [
+	{
+
+	},
+	{
+
+	},
+	{
+
+	},
+	{
+
+	}
+];
+
 // Define a placeholder array named "newCard".
+
+var newCard = [];
+
+// Create an object that holds clusters of HTML code for formatting cards
+
+var cluster = {
+								article: 					"<article class='card' style='background:",
+								styleMid: 				"; color:",
+								styleEnd: 				"'>",
+								endArticle: 			"</article>",
+
+								h3: 							"<h3 class='cardName'>",
+								endH3: 						"</h3>",
+
+								sectionText: 			"<section class='card-text'>",
+								endSectionText: 	"</section>",
+
+								divDelete: 				"<div class='cardDelete'>",
+								endDivDelete: 		"</div>"
+							}
 
 // Create a function [createNewCard()] that...
  // a. takes values from the form fields on the sidebar and dumps them in "newCard"
