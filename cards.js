8d5a5126e448ee70db4dbad821c91817729dfd38
Chase Ramsey <ramsey.chase@gmail.com>
2016-04-19 20:49:41 -0500

    Created function dealCards that builds cards from charCards and puts them in the DOM

--- cards.js
+++ cards.js
@@ -4,28 +4,28 @@
 
 var charCards = [
 	{
-		name: Heathcliff;
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.";
-		bgcolor: tomato;
-		font: black;
+		name: "Heathcliff",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.",
+		bgcolor: "tomato",
+		font: "black"
 	},
 	{
-		name: Mario;
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.";
-		bgcolor: blue;
-		font: white;
+		name: "Mario",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.",
+		bgcolor: "blue",
+		font: "white"
 	},
 	{
-		name: Snoopy;
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.";
-		bgcolor: green;
-		font: orange;
+		name: "Snoopy",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.",
+		bgcolor: "green",
+		font: "orange"
 	},
 	{
-		name: Arnold;
-		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.";
-		bgcolor: pink;
-		font: yellow;
+		name: "Arnold",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fermentum eros mauris, a rhoncus libero dictum id. Sed hendrerit fermentum consectetur. Nam ullamcorper non urna elementum tristique. Donec cursus faucibus congue. Nulla consequat, lacus vel lacinia tristique, erat sem venenatis nunc, quis sagittis magna lorem vitae quam. Sed sit amet augue id nibh dictum ultricies. Nulla cursus at purus quis faucibus.",
+		bgcolor: "pink",
+		font: "yellow"
 	}
 ];
 
@@ -46,15 +46,36 @@ var endH3 = 					"</h3>";
 var sectionText = 		"<section class='card-text'>";
 var endSectionText = 	"</section>";
 
-var divDelete = 			"<div class='cardDelete'>";
-var endDivDelete = 		"</div>";
+var divDelete = 			"<div class='cardDelete'>Delete Card</div>";
+
+// Create a function that...
+	// a. introduces all objects in "charCards" into the DOM with formatting
+
+function dealCards() {
+	for (var i = 0; i < charCards.length; i++) {
+		var build = charCards[i];
+		var cardTable = document.getElementById("mainContent");
+		cardTable.innerHTML  += article + build.bgcolor + styleMid + build.font + styleEnd +
+														h3 + build.name + endH3 +
+														sectionText + build.text + endSectionText +
+														divDelete + endArticle;
+	}
+}
+
 
 // Create a function [createNewCard()] that...
  // a. takes values from the form fields on the sidebar and dumps them in "newCard"
  // b. copies the newly created object into "charCards" at the beginning
- // c. introduces the new object to the DOM
+ // c. runs print function and inputs inputs all objects into the DOM
 
 // Create a function [deleteCard()] that...
 	// a. listens for a click on the card footer
 	// b. removes the clicked card from "charCards"
 	// c. removes the clicked card from the DOM
+
+
+
+
+
+
+
