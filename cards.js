69f78c5e1d64445c3a15a9cc07b47d6b6d40699b
Chase Ramsey <ramsey.chase@gmail.com>
2016-04-19 19:30:50 -0500

    Wrote basic outline for program in comments

---
+++ cards.js
@@ -0,0 +1,15 @@
+
+// Create an array of objects named "charCards" and populate the array with a few premade objects.
+// Include key/value pairs for all card specifications that need to be stored.
+
+// Define a placeholder array named "newCard".
+
+// Create a function [createNewCard()] that...
+ // a. takes values from the form fields on the sidebar and dumps them in "newCard"
+ // b. copies the newly created object into "charCards" at the beginning
+ // c. introduces the new object to the DOM
+
+// Create a function [deleteCard()] that...
+	// a. listens for a click on the card footer
+	// b. removes the clicked card from "charCards"
+	// c. removes the clicked card from the DOM
